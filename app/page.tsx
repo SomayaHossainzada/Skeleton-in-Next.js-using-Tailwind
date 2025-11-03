7a855798e28a90c800fb0a59a2bc1bb6fdbf5b41
saeedhei <saeedhei@gmail.com>
2025-11-03 09:49:54 +0100

    skleton updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,25 @@
-"use client";
 
-import React, { useEffect, useState } from "react";
-import Skeleton from "./components/Skeleton";
-import Image from "next/image";
-const Home = () => {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export default async function UsersPage() {
+  const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+  await sleep(2000);
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
   return (
-    <div
-      className="min-h-screen items-center flex justify-center 
-text-white bg-black"
-    >
-      <div
-        className="max-w-lg w-full p-6 rounded-xl bg-gray-900
-      shadow-lg"
-      >
-        {loading ? (
-          <Skeleton />
-        ) : (
-          <div className="w-full p-4 space-y-4">
-            <h1 className="text-xl font-bold">Welcome to the Future</h1>
-            <p className="text-gray-400 mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-              quasi in et aliquid recusandae accusamus accusantium sit libero.
-            </p>
-            <div
-              className="w-full h-64 relative bg-gray-700 rounded-lg
-            overflow-hidden"
-            >
-              <Image
-                src="./shoe1.webp"
-                alt="shoe"
-                width={500}
-                height={300}
-                className="absolute insert-0 w-full object-cover
-              transition-opacity duration-500"
-              />
-            </div>
-          </div>
-        )}
-      </div>
+    <div className="p-4">
+      {users.map((user) => (
+        <div key={user.id} className="border-b py-2 flex flex-col space-y-2">
+          <h2 className="font-bold text-lg leading-snug">{user.name}</h2>
+          <p className="text-sm text-gray-600">{user.email}</p>
+        </div>
+      ))}
     </div>
   );
-};
-
-export default Home;
+}
